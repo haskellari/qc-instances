be691858f18171e8a1afbb70ebb4468f568791a3
PHO <pho@cielonegro.org>
2012-01-18 12:15:09 +0900

    Provide 'CoArbitrary' instances for types.

--- src/Test/QuickCheck/Instances.hs
+++ src/Test/QuickCheck/Instances.hs
@@ -4,8 +4,7 @@
 {-| 
 Instances are provided for the types in the packages:
 
- * array - We only provide instances for arrays inexed by
-   Integral types.
+ * array
 
  * bytestring
 
@@ -59,6 +58,13 @@ instance (Array.IArray Array.UArray e, Array.Ix i, Arbitrary i, Arbitrary e)
     arbitrary = arbitraryArray
     shrink    = shrinkArray
 
+instance (Array.Ix i, CoArbitrary i, CoArbitrary e) => CoArbitrary (Array.Array i e) where
+    coarbitrary = coarbitraryArray
+
+instance (Array.IArray Array.UArray e, Array.Ix i, CoArbitrary i, CoArbitrary e)
+        => CoArbitrary (Array.UArray i e) where
+    coarbitrary = coarbitraryArray
+
 arbitraryArray :: (Array.IArray a e, Array.Ix i, Arbitrary i, Arbitrary e) => Gen (a i e)
 arbitraryArray = do
       b1 <- arbitrary
@@ -75,6 +81,9 @@ shrinkArray a =
         elmss  = shrink <$> Array.elems a
     in Array.listArray bounds <$> elmss
 
+coarbitraryArray :: (Array.IArray a e, Array.Ix i, CoArbitrary i, CoArbitrary e)
+                    => a i e -> Gen c -> Gen c
+coarbitraryArray = coarbitrary . Array.assocs
 
 -- ByteString
 instance Arbitrary BS.ByteString where
@@ -85,6 +94,12 @@ instance Arbitrary BL.ByteString where
     arbitrary = BL.pack <$> arbitrary
     shrink xs = BL.pack <$> shrink (BL.unpack xs)
 
+instance CoArbitrary BS.ByteString where
+    coarbitrary = coarbitrary . BS.unpack
+
+instance CoArbitrary BL.ByteString where
+    coarbitrary = coarbitrary . BL.unpack
+
 -- Text
 instance Arbitrary TS.Text where
     arbitrary = TS.pack <$> arbitrary
@@ -94,27 +109,47 @@ instance Arbitrary TL.Text where
     arbitrary = TL.pack <$> arbitrary
     shrink xs = TL.pack <$> shrink (TL.unpack xs)
 
--- Containers
+instance CoArbitrary TS.Text where
+    coarbitrary = coarbitrary . TS.unpack
 
+instance CoArbitrary TL.Text where
+    coarbitrary = coarbitrary . TL.unpack
+
+-- Containers
 instance Arbitrary a => Arbitrary (IntMap.IntMap a) where
     arbitrary = IntMap.fromList <$> arbitrary
     shrink m = IntMap.fromList <$> shrink (IntMap.toList m)
+
+instance CoArbitrary a => CoArbitrary (IntMap.IntMap a) where
+    coarbitrary = coarbitrary . IntMap.toList
 
 instance Arbitrary IntSet.IntSet where
     arbitrary = IntSet.fromList <$> arbitrary
     shrink set = IntSet.fromList <$> shrink (IntSet.toList set)
 
+instance CoArbitrary IntSet.IntSet where
+    coarbitrary = coarbitrary . IntSet.toList
+
 instance (Ord k, Arbitrary k, Arbitrary v) => Arbitrary (Map.Map k v) where
     arbitrary = Map.fromList <$> arbitrary
     shrink m = Map.fromList <$> shrink (Map.toList m)
+
+instance (CoArbitrary k, CoArbitrary v) => CoArbitrary (Map.Map k v) where
+    coarbitrary = coarbitrary . Map.toList
 
 instance Arbitrary a => Arbitrary (Seq.Seq a) where
     arbitrary = Seq.fromList <$> arbitrary
     shrink xs = Seq.fromList <$> shrink (toList xs)
 
+instance CoArbitrary a => CoArbitrary (Seq.Seq a) where
+    coarbitrary = coarbitrary . toList
+
 instance (Ord a, Arbitrary a) => Arbitrary (Set.Set a) where
     arbitrary = Set.fromList <$> arbitrary
     shrink set = Set.fromList <$> shrink (Set.toList set)
+
+instance CoArbitrary a => CoArbitrary (Set.Set a) where
+    coarbitrary = coarbitrary . Set.toList
 
 instance Arbitrary a => Arbitrary (Tree.Tree a) where
     arbitrary = sized $ \n ->
@@ -129,14 +164,23 @@ instance Arbitrary a => Arbitrary (Tree.Tree a) where
          return $ Tree.Node val nodes
     shrink (Tree.Node val forest) =
         Tree.Node <$> shrink val <*> shrink forest
-         
--- old-time
 
+instance CoArbitrary a => CoArbitrary (Tree.Tree a) where
+    coarbitrary (Tree.Node val forest) =
+        coarbitrary val >< coarbitrary forest
+
+-- old-time
 instance Arbitrary OldTime.Month where
     arbitrary = arbitraryBoundedEnum
 
+instance CoArbitrary OldTime.Month where
+    coarbitrary = coarbitraryEnum
+
 instance Arbitrary OldTime.Day where
     arbitrary = arbitraryBoundedEnum
+
+instance CoArbitrary OldTime.Day where
+    coarbitrary = coarbitraryEnum
 
 instance Arbitrary OldTime.ClockTime where
     arbitrary =
@@ -146,33 +190,69 @@ instance Arbitrary OldTime.ClockTime where
         [ OldTime.TOD s' p  | s' <- shrink s ] ++
         [ OldTime.TOD s  p' | p' <- shrink p ]
 
+instance CoArbitrary OldTime.ClockTime where
+    coarbitrary (OldTime.TOD s p) =
+        coarbitrary s >< coarbitrary p
+
 instance Arbitrary OldTime.TimeDiff where
     -- a bit of a cheat ...
     arbitrary =
         OldTime.normalizeTimeDiff <$>
            (OldTime.diffClockTimes <$> arbitrary <*> arbitrary)
-    shrink td =
-        [ td { OldTime.tdYear    = y' } | y' <- shrink (OldTime.tdYear    td) ] ++
-        [ td { OldTime.tdMonth   = m' } | m' <- shrink (OldTime.tdMonth   td) ] ++
-        [ td { OldTime.tdDay     = d' } | d' <- shrink (OldTime.tdDay     td) ] ++
-        [ td { OldTime.tdHour    = h' } | h' <- shrink (OldTime.tdHour    td) ] ++
-        [ td { OldTime.tdMin     = m' } | m' <- shrink (OldTime.tdMin     td) ] ++
-        [ td { OldTime.tdSec     = s' } | s' <- shrink (OldTime.tdSec     td) ] ++
-        [ td { OldTime.tdPicosec = p' } | p' <- shrink (OldTime.tdPicosec td) ]
+    shrink td@(OldTime.TimeDiff year month day hour minute second picosec) =
+        [ td { OldTime.tdYear    = y' } | y' <- shrink year    ] ++
+        [ td { OldTime.tdMonth   = m' } | m' <- shrink month   ] ++
+        [ td { OldTime.tdDay     = d' } | d' <- shrink day     ] ++
+        [ td { OldTime.tdHour    = h' } | h' <- shrink hour    ] ++
+        [ td { OldTime.tdMin     = m' } | m' <- shrink minute  ] ++
+        [ td { OldTime.tdSec     = s' } | s' <- shrink second  ] ++
+        [ td { OldTime.tdPicosec = p' } | p' <- shrink picosec ]
+
+instance CoArbitrary OldTime.TimeDiff where
+    coarbitrary (OldTime.TimeDiff year month day hour minute second picosec) =
+        coarbitrary year    ><
+        coarbitrary month   ><
+        coarbitrary day     ><
+        coarbitrary hour    ><
+        coarbitrary minute  ><
+        coarbitrary second  ><
+        coarbitrary picosec
 
 -- UTC only
 instance Arbitrary OldTime.CalendarTime where
     arbitrary = OldTime.toUTCTime <$> arbitrary
 
--- time
+instance CoArbitrary OldTime.CalendarTime where
+    coarbitrary (OldTime.CalendarTime
+                        year month day hour minute second picosec
+                        wDay yDay tzName tz isDST) =
+        coarbitrary year    ><
+        coarbitrary month   ><
+        coarbitrary day     ><
+        coarbitrary hour    ><
+        coarbitrary minute  ><
+        coarbitrary second  ><
+        coarbitrary picosec ><
+        coarbitrary wDay    ><
+        coarbitrary yDay    ><
+        coarbitrary tzName  ><
+        coarbitrary tz      ><
+        coarbitrary isDST
 
+-- time
 instance Arbitrary Time.Day where
     arbitrary = Time.ModifiedJulianDay <$> (2000 +) <$> arbitrary
     shrink    = (Time.ModifiedJulianDay <$>) . shrink . Time.toModifiedJulianDay
 
+instance CoArbitrary Time.Day where
+    coarbitrary = coarbitrary . Time.toModifiedJulianDay
+
 instance Arbitrary Time.UniversalTime where
     arbitrary = Time.ModJulianDate <$> (2000 +) <$> arbitrary
     shrink    = (Time.ModJulianDate <$>) . shrink . Time.getModJulianDate
+
+instance CoArbitrary Time.UniversalTime where
+    coarbitrary = coarbitrary . Time.getModJulianDate
 
 instance Arbitrary Time.DiffTime where
     arbitrary = arbitrarySizedFractional
@@ -182,18 +262,28 @@ instance Arbitrary Time.DiffTime where
     shrink    = (fromRational <$>) . shrink . toRational
 #endif
 
+instance CoArbitrary Time.DiffTime where
+    coarbitrary = coarbitraryReal
+
 instance Arbitrary Time.UTCTime where
     arbitrary =
         Time.UTCTime
         <$> arbitrary
         <*> (fromRational . toRational <$> choose (0::Double, 86400))
-    shrink ut =
-        [ ut { Time.utctDay     = d'  } | d'  <- shrink (Time.utctDay     ut) ] ++
-        [ ut { Time.utctDayTime = dt' } | dt' <- shrink (Time.utctDayTime ut) ]
+    shrink ut@(Time.UTCTime day dayTime) =
+        [ ut { Time.utctDay     = d' } | d' <- shrink day     ] ++
+        [ ut { Time.utctDayTime = t' } | t' <- shrink dayTime ]
+
+instance CoArbitrary Time.UTCTime where
+    coarbitrary (Time.UTCTime day dayTime) =
+        coarbitrary day >< coarbitrary dayTime
 
 instance Arbitrary Time.NominalDiffTime where
     arbitrary = arbitrarySizedFractional
     shrink    = shrinkRealFrac
+
+instance CoArbitrary Time.NominalDiffTime where
+    coarbitrary = coarbitraryReal
 
 instance Arbitrary Time.TimeZone where
     arbitrary =
@@ -201,10 +291,14 @@ instance Arbitrary Time.TimeZone where
          <$> choose (-12*60*60,12*60*60) -- utc offset (s)
          <*> arbitrary -- is summer time
          <*> (sequence . replicate 4 $ choose ('A','Z'))
-    shrink tz =
-        [ tz { Time.timeZoneMinutes    = m' } | m' <- shrink (Time.timeZoneMinutes    tz) ] ++
-        [ tz { Time.timeZoneSummerOnly = s' } | s' <- shrink (Time.timeZoneSummerOnly tz) ] ++
-        [ tz { Time.timeZoneName       = n' } | n' <- shrink (Time.timeZoneName       tz) ]
+    shrink tz@(Time.TimeZone minutes summerOnly name) =
+        [ tz { Time.timeZoneMinutes    = m' } | m' <- shrink minutes    ] ++
+        [ tz { Time.timeZoneSummerOnly = s' } | s' <- shrink summerOnly ] ++
+        [ tz { Time.timeZoneName       = n' } | n' <- shrink name       ]
+
+instance CoArbitrary Time.TimeZone where
+    coarbitrary (Time.TimeZone minutes summerOnly name) =
+        coarbitrary minutes >< coarbitrary summerOnly >< coarbitrary name
 
 instance Arbitrary Time.TimeOfDay where
     arbitrary =
@@ -212,28 +306,40 @@ instance Arbitrary Time.TimeOfDay where
          <$> choose (0, 23) -- hour
          <*> choose (0, 59) -- minute
          <*> (fromRational . toRational <$> choose (0::Double, 60)) -- picoseconds, via double
-    shrink tod =
-        [ tod { Time.todHour = h' } | h' <- shrink (Time.todHour tod) ] ++
-        [ tod { Time.todMin  = m' } | m' <- shrink (Time.todMin  tod) ] ++
-        [ tod { Time.todSec  = s' } | s' <- shrink (Time.todSec  tod) ]
+    shrink tod@(Time.TimeOfDay hour minute second) =
+        [ tod { Time.todHour = h' } | h' <- shrink hour   ] ++
+        [ tod { Time.todMin  = m' } | m' <- shrink minute ] ++
+        [ tod { Time.todSec  = s' } | s' <- shrink second ]
+
+instance CoArbitrary Time.TimeOfDay where
+    coarbitrary (Time.TimeOfDay hour minute second) =
+        coarbitrary hour >< coarbitrary minute >< coarbitrary second
 
 instance Arbitrary Time.LocalTime where
     arbitrary =
         Time.LocalTime
          <$> arbitrary
          <*> arbitrary
-    shrink lt =
-        [ lt { Time.localDay       = d' } | d' <- shrink (Time.localDay       lt) ] ++
-        [ lt { Time.localTimeOfDay = t' } | t' <- shrink (Time.localTimeOfDay lt) ]
+    shrink lt@(Time.LocalTime day tod) =
+        [ lt { Time.localDay       = d' } | d' <- shrink day ] ++
+        [ lt { Time.localTimeOfDay = t' } | t' <- shrink tod ]
+
+instance CoArbitrary Time.LocalTime where
+    coarbitrary (Time.LocalTime day tod) =
+        coarbitrary day >< coarbitrary tod
 
 instance Arbitrary Time.ZonedTime where
     arbitrary =
         Time.ZonedTime
          <$> arbitrary
          <*> arbitrary
-    shrink zt =
-        [ zt { Time.zonedTimeToLocalTime = l' } | l' <- shrink (Time.zonedTimeToLocalTime zt) ] ++
-        [ zt { Time.zonedTimeZone        = z' } | z' <- shrink (Time.zonedTimeZone        zt) ]
+    shrink zt@(Time.ZonedTime lt zone) =
+        [ zt { Time.zonedTimeToLocalTime = l' } | l' <- shrink lt   ] ++
+        [ zt { Time.zonedTimeZone        = z' } | z' <- shrink zone ]
+
+instance CoArbitrary Time.ZonedTime where
+    coarbitrary (Time.ZonedTime lt zone) =
+        coarbitrary lt >< coarbitrary zone
 
 instance Arbitrary Time.AbsoluteTime where
     arbitrary =
@@ -243,13 +349,22 @@ instance Arbitrary Time.AbsoluteTime where
     shrink at =
         (`Time.addAbsoluteTime` at) <$> shrink (Time.diffAbsoluteTime at Time.taiEpoch)
 
+instance CoArbitrary Time.AbsoluteTime where
+    coarbitrary = coarbitrary . flip Time.diffAbsoluteTime Time.taiEpoch
+
 -- WARNING: from base, should be moved to QC library
 instance Arbitrary Ordering where
     arbitrary = arbitraryBoundedEnum
 
+instance CoArbitrary Ordering where
+    coarbitrary = coarbitraryEnum
+
 instance Fixed.HasResolution a => Arbitrary (Fixed.Fixed a) where
     arbitrary = arbitrarySizedFractional
     shrink    = shrinkRealFrac
+
+instance Fixed.HasResolution a => CoArbitrary (Fixed.Fixed a) where
+    coarbitrary = coarbitraryReal
 
 -- WARNING: should be moved to QC library
 arbitraryBoundedEnum :: (Bounded a, Enum a) => Gen a
@@ -258,3 +373,6 @@ arbitraryBoundedEnum =
          mx = maxBound `asTypeOf` mn
      n <- choose (fromEnum mn, fromEnum mx)
      return (toEnum n `asTypeOf` mn)
+
+coarbitraryEnum :: Enum a => a -> Gen c -> Gen c
+coarbitraryEnum = variant . fromEnum
