c99f21354cf958338f658acbf72f20f9aa467469
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-06-04 21:24:33 +0300

    Resolve #4: Add UUID instances

--- src/Test/QuickCheck/Instances.hs
+++ src/Test/QuickCheck/Instances.hs
@@ -38,6 +38,7 @@ import Data.Hashable
 import Data.Ratio
 import Test.QuickCheck
 import Test.QuickCheck.Function
+import Data.Word (Word32)
 
 import qualified Data.Array.IArray as Array
 import qualified Data.Array.Unboxed as Array
@@ -64,6 +65,7 @@ import qualified Data.Vector.Storable as SVector
 import qualified Data.Vector.Generic as GVector
 import qualified Data.Vector.Unboxed as UVector
 import qualified System.Time as OldTime
+import qualified Data.UUID.Types as UUID
 
 import Debug.Trace
 
@@ -521,3 +523,19 @@ instance CoArbitrary b => CoArbitrary (Tagged.Tagged a b) where
 
 instance Function b => Function (Tagged.Tagged a b) where
     function = functionMap Tagged.unTagged Tagged.Tagged
+
+-- uuid
+
+uuidFromWords :: (Word32, Word32, Word32, Word32) -> UUID.UUID
+uuidFromWords (a,b,c,d) = UUID.fromWords a b c d
+
+-- | Uniform distribution.
+instance Arbitrary UUID.UUID where
+    arbitrary = uuidFromWords <$> arbitrary
+    shrink = map uuidFromWords . shrink . UUID.toWords
+
+instance CoArbitrary UUID.UUID where
+    coarbitrary = coarbitrary . UUID.toWords
+
+instance Function UUID.UUID where
+    function = functionMap UUID.toWords uuidFromWords
