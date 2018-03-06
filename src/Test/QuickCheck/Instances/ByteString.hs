e6cac6386065f5081ed3b457a69af84bcbbf8275
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2018-03-06 18:15:32 +0100

    Add instances for ShortByteString

--- src/Test/QuickCheck/Instances/ByteString.hs
+++ src/Test/QuickCheck/Instances/ByteString.hs
@@ -10,6 +10,9 @@ import Test.QuickCheck
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
+#if MIN_VERSION_bytestring(0,10,4)
+import qualified Data.ByteString.Short as SBS
+#endif
 
 -------------------------------------------------------------------------------
 -- bytestring
@@ -23,8 +26,19 @@ instance Arbitrary BL.ByteString where
     arbitrary = BL.pack <$> arbitrary
     shrink xs = BL.pack <$> shrink (BL.unpack xs)
 
+#if MIN_VERSION_bytestring(0,10,4)
+instance Arbitrary SBS.ShortByteString where
+    arbitrary = SBS.pack <$> arbitrary
+    shrink xs = SBS.pack <$> shrink (SBS.unpack xs)
+#endif
+
 instance CoArbitrary BS.ByteString where
     coarbitrary = coarbitrary . BS.unpack
 
 instance CoArbitrary BL.ByteString where
     coarbitrary = coarbitrary . BL.unpack
+
+#if MIN_VERSION_bytestring(0,10,4)
+instance CoArbitrary SBS.ShortByteString where
+    coarbitrary = coarbitrary . SBS.unpack
+#endif
