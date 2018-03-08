0948bfa7503835861b05abe6a93b8577a777b4b6
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-03-08 22:36:37 +0200

    Prepare for 0.3.17 release
    Add Function ByteString instances

--- src/Test/QuickCheck/Instances/ByteString.hs
+++ src/Test/QuickCheck/Instances/ByteString.hs
@@ -22,23 +22,32 @@ instance Arbitrary BS.ByteString where
     arbitrary = BS.pack <$> arbitrary
     shrink xs = BS.pack <$> shrink (BS.unpack xs)
 
+instance CoArbitrary BS.ByteString where
+    coarbitrary = coarbitrary . BS.unpack
+
+instance Function BS.ByteString where
+    function = functionMap BS.unpack BS.pack
+
+
 instance Arbitrary BL.ByteString where
     arbitrary = BL.pack <$> arbitrary
     shrink xs = BL.pack <$> shrink (BL.unpack xs)
+
+instance CoArbitrary BL.ByteString where
+    coarbitrary = coarbitrary . BL.unpack
+
+instance Function BL.ByteString where
+    function = functionMap BL.unpack BL.pack
+
 
 #if MIN_VERSION_bytestring(0,10,4)
 instance Arbitrary SBS.ShortByteString where
     arbitrary = SBS.pack <$> arbitrary
     shrink xs = SBS.pack <$> shrink (SBS.unpack xs)
-#endif
 
-instance CoArbitrary BS.ByteString where
-    coarbitrary = coarbitrary . BS.unpack
-
-instance CoArbitrary BL.ByteString where
-    coarbitrary = coarbitrary . BL.unpack
-
-#if MIN_VERSION_bytestring(0,10,4)
 instance CoArbitrary SBS.ShortByteString where
     coarbitrary = coarbitrary . SBS.unpack
+
+instance Function SBS.ShortByteString where
+    function = functionMap SBS.unpack SBS.pack
 #endif
