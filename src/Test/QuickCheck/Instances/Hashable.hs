6436d92bb03633fd822f639d550a4d4e9bf23a9a
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-10-31 22:47:17 +0200

    Support hashable-1.4

--- src/Test/QuickCheck/Instances/Hashable.hs
+++ src/Test/QuickCheck/Instances/Hashable.hs
@@ -6,7 +6,12 @@ module Test.QuickCheck.Instances.Hashable () where
 import Prelude ()
 import Test.QuickCheck.Instances.CustomPrelude
 
-import Data.Hashable (Hashable, Hashed, hash, hashed)
+import Data.Hashable (Hashable, Hashed, hashed)
+#if MIN_VERSION_hashable(1,4,0)
+import Data.Hashable (hashedHash)
+#else
+import Data.Hashable (hash)
+#endif
 
 import Test.QuickCheck
 
@@ -19,5 +24,9 @@ instance (Hashable a, Arbitrary a) => Arbitrary (Hashed a) where
     arbitrary = hashed <$> arbitrary
 
 instance CoArbitrary (Hashed a) where
+#if MIN_VERSION_hashable(1,4,0)
+    coarbitrary x = coarbitrary (hashedHash x :: Int)
+#else
     coarbitrary x = coarbitrary (hash x :: Int)
+#endif
 #endif
