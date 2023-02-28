23664cb48b8c33f4e85a5abdf8d338db137160da
Oleg Grenrus <oleg.grenrus@iki.fi>
2023-02-28 22:44:11 +0200

    Support OneTuple-0.4

--- src/Test/QuickCheck/Instances/Solo.hs
+++ src/Test/QuickCheck/Instances/Solo.hs
@@ -5,13 +5,18 @@ module Test.QuickCheck.Instances.Solo () where
 import Prelude ()
 import Test.QuickCheck.Instances.CustomPrelude
 
+#if MIN_VERSION_OneTuple(0,4,0)
+import Data.Tuple.Solo (Solo (MkSolo), getSolo)
+#else
 import Data.Tuple.Solo (Solo (Solo), getSolo)
+#define MkSolo Solo
+#endif
 
 import Test.QuickCheck
 
 instance Arbitrary1 Solo where
-  liftArbitrary = fmap Solo
-  liftShrink shr = map Solo . shr . getSolo
+  liftArbitrary = fmap MkSolo
+  liftShrink shr = map MkSolo . shr . getSolo
 
 instance Arbitrary a => Arbitrary (Solo a) where
   arbitrary = arbitrary1
@@ -21,4 +26,4 @@ instance CoArbitrary a => CoArbitrary (Solo a) where
   coarbitrary = coarbitrary . getSolo
 
 instance Function a => Function (Solo a) where
-  function = functionMap getSolo Solo
+  function = functionMap getSolo MkSolo
